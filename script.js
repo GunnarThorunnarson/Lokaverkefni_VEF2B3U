083eecaec903b86dc9d02727eb3ffc936a9bdc0f
erla97 <erlaos207@nemi.tskoli.is>
2017-11-24 23:15:57 +0000

    Bæting á kóða
    t.d. var þetta gert:
    - lagað fyrri kóða, gert hann snyrtilegri og minni
    - Bætt við 3 icons og birt þau
    - Bætt við hljóði, látið það heyrast á ákveðnum tíma

--- script.js
+++ script.js
@@ -5,27 +5,27 @@ var canvas = document.getElementById("myCanvas"); //Sótt í canvas
 var ctx = canvas.getContext("2d"); //Þetta notað til þess að teikna í canvasinn
 
 //Breytur Skilgreindar
+var alert;
 var paddleHeight = 10;
-var paddleWidth = 80;
+var paddleWidth = 90;
 var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 var ballRadius = 14;
-var x = canvas.width/2; //x fyrir staðsetningu á x-ás
-var y = canvas.height-30; //y fyrir staðsetningu á y-ás
-var x2 = 100;
-var y2 = 200;
-var dx = 2;
-var dy = -5;
-var dx2 = 2;
-var dy2 = 4;
-var x3 = 460;
-var y3 = 100;
-var dx3 = 3;
-var dy3 = 5;
+var totalScore = 0;
+var counter = 0;
+var c1 = 0;
+var c2 = 0;
+var bounce = new Audio('Sound/bounce.mp3');
+
+//Fylki sem inniheldur liti
+var colors = ["#3498db", "#9b59b6", "#f1c40f", "#e67e22", "#2ecc71", "#f39c12", "#1abc9c"];
+c1 = Math.floor(Math.random() * 7);
+c2 = Math.floor(Math.random() * 7);
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 function keyDownHandler(e) {
     if(e.keyCode == 39) {
@@ -45,40 +45,72 @@ function keyUpHandler(e) {
     }
 }
 
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
+    }
+}
+
+document.addEventListener("Bounce", function() {
+	if (!bounce.ended) {
+		bounce.pause();
+		bounce.currentTime = 0;
+	}
+	bounce.play();
+});
+
+var bEvent = new CustomEvent("Bounce");
+
 var Ball = {
-	create: function () {
+	create: function (bx, by, bdx,  bdy) {
 		var newBall = Object.create(this);
+		newBall.x = bx;
+		newBall.y = by;
+		newBall.dx = bdx;
+		newBall.dy = bdy;
 		return newBall;
 	},
-	collision: function (x, y, dx, dy) {
-		if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-	        dx = -dx;
+
+	collision: function () {
+		if(this.x + this.dx > canvas.width-ballRadius || this.x + this.dx < ballRadius) {
+	        this.dx = -this.dx;
 	    }
-	    if(y + dy < ballRadius) {
-	        dy = -dy;
+	    if(this.y + this.dy < ballRadius) {
+	        this.dy = -this.dy;
 	    } 
-		else if(y + dy > canvas.height-ballRadius) {
-		    if(x > paddleX && x < paddleX + paddleWidth) {
-		        dy = -dy;
+		else if(this.y - 5 + this.dy > canvas.height-ballRadius) {
+		    if(this.x > paddleX - 10 && this.x < paddleX + paddleWidth + 10) {
+		        this.dy = -this.dy;
+		        totalScore = totalScore + 5;
+		        document.getElementById("score").innerHTML = "Score: " + totalScore;
+		        counter = counter + 1;
+	        	this.dy = this.dy + 0.05;
+	        	this.dx = this.dx + 0.05;
+		        document.dispatchEvent(bEvent);
 		    }
 		    else {
 		        alert("GAME OVER");
 		        document.location.reload();
 		    }
 		}
+
+		this.x += this.dx;
+    	this.y += this.dy;
 	},
-	drawBall: function (x, y, color, dx, dy) {
+
+	drawBall: function (color) {
 		ctx.beginPath();
-	    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+	    ctx.arc(this.x, this.y, ballRadius, 0, Math.PI*2);
 	    ctx.fillStyle = color;
 	    ctx.fill();
 	    ctx.closePath();
+	    this.collision();
 	}
 };
 
-var ball1 = Ball.create();
-var ball2 = Ball.create();
-var ball3 = Ball.create();
+var ball1 = Ball.create(100, 200, 2, -5);
+var ball2 = Ball.create(400, 300, 3, 4);
 
 function drawPaddle() {
     ctx.beginPath();
@@ -88,95 +120,18 @@ function drawPaddle() {
     ctx.closePath();
 }
 
-function collision(x, y, dx, dy) {
-	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-        dx = -dx;
-    }
-    if(y + dy < ballRadius) {
-        dy = -dy;
-    } 
-	else if(y + dy > canvas.height-ballRadius) {
-	    if(x > paddleX && x < paddleX + paddleWidth) {
-	        dy = -dy;
-	    }
-	    else {
-	        alert("GAME OVER");
-	        document.location.reload();
-	    }
-	}
-}
-
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ball1.drawBall(x, y, "#9b59b6", dx, dy);
-    ball2.drawBall(x2, y2, "#e67e22", dx, dy);
-    ball3.drawBall(x3, y3, "#27ae60", dx, dy);
+    ball1.drawBall(colors[c1]);
+    ball2.drawBall(colors[c2]);
     drawPaddle();
 
-    //Bolti 1
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-	    dx = -dx;
-	}
-    if(y + dy < ballRadius) {
-        dy = -dy;
-    }
-	else if(y + dy > canvas.height-ballRadius) {
-	    if(x > paddleX && x < paddleX + paddleWidth) {
-	        dy = -dy;
-	    }
-	    else {
-	        alert("GAME OVER");
-	        document.location.reload();
-	    }
-	}
-
-	//Bolti 2
-	if(x2 + dx2 > canvas.width-ballRadius || x2 + dx2 < ballRadius) {
-        dx2 = -dx2;
-    }
-    if(y2 + dy2 < ballRadius) {
-        dy2 = -dy2;
-    } 
-	else if(y2 + dy2 > canvas.height-ballRadius) {
-	    if(x2 > paddleX && x2 < paddleX + paddleWidth) {
-	        dy2 = -dy2;
-	    }
-	    else {
-	        alert("GAME OVER");
-	        document.location.reload();
-	    }
-	}
-
-	//Bolti 3
-	if(x3 + dx3 > canvas.width-ballRadius || x3 + dx3 < ballRadius) {
-        dx3 = -dx3;
-    }
-    if(y3 + dy3 < ballRadius) {
-        dy3 = -dy3;
-    } 
-	else if(y3 + dy3> canvas.height-ballRadius) {
-	    if(x3 > paddleX && x3 < paddleX + paddleWidth) {
-	        dy3= -dy3;
-	    }
-	    else {
-	        alert("GAME OVER");
-	        document.location.reload();
-	    }
-	}
-
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
-    	paddleX += 10;
+    	paddleX += 15;
 	}
 	else if(leftPressed && paddleX > 0) {
-	    paddleX -= 10;
+	    paddleX -= 15;
 	}
-
-	x += dx;
-    y += dy;
-    x2 += dx2;
-    y2 += dy2;
-    x3 += dx3;
-    y3 += dy3;
 }
 
 setInterval(draw, 20);
